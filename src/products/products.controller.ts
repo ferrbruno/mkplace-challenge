7183fbd0301c1b5a7ef3975e8544d0803f36e630
Bruno Ferreira <bruno.ferr.dev@gmail.com>
2023-02-09 19:31:07 -0300

    feat: added prisma service to products module :sparkles:

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -7,10 +7,12 @@ import {
   Param,
   Delete,
 } from '@nestjs/common';
+import { ApiTags } from '@nestjs/swagger';
+import { Prisma } from '@prisma/client';
 import { ProductsService } from './products.service';
 import { CreateProductDto } from './dto/create-product.dto';
+import { SearchProductDto } from './dto/search-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
-import { ApiTags } from '@nestjs/swagger';
 
 const controllerName = 'products';
 
@@ -20,8 +22,23 @@ export class ProductsController {
   constructor(private readonly productsService: ProductsService) {}
 
   @Post()
-  create(@Body() createProductDto: CreateProductDto) {
-    return this.productsService.create(createProductDto);
+  create(@Body() { brand, name, price_range, seller }: CreateProductDto) {
+    return this.productsService.create({
+      name,
+      price_range,
+      brand: {
+        connectOrCreate: {
+          where: { name: brand },
+          create: { name: brand },
+        },
+      },
+      seller: {
+        connectOrCreate: {
+          where: { name: seller },
+          create: { name: seller },
+        },
+      },
+    });
   }
 
   @Get()
@@ -29,18 +46,84 @@ export class ProductsController {
     return this.productsService.findAll();
   }
 
+  @Post('/search')
+  find(@Body() { brand, name, price_range, seller }: SearchProductDto) {
+    const searhFilter: Prisma.Enumerable<Prisma.ProductWhereInput> = [];
+
+    if (brand) {
+      searhFilter.push({ brand: { name: { contains: brand } } });
+    }
+
+    if (seller) {
+      searhFilter.push({ seller: { name: { contains: seller } } });
+    }
+
+    if (name) {
+      searhFilter.push({ name: { contains: name } });
+    }
+
+    if (price_range) {
+      searhFilter.push({ price_range });
+    }
+
+    return this.productsService.find({
+      where: {
+        OR: searhFilter,
+      },
+      include: {
+        brand: true,
+        seller: true,
+      },
+    });
+  }
+
   @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.productsService.findOne(+id);
+  findOne(@Param('id') id: number) {
+    return this.productsService.findById(id);
   }
 
   @Patch(':id')
-  update(@Param('id') id: string, @Body() updateProductDto: UpdateProductDto) {
-    return this.productsService.update(+id, updateProductDto);
+  update(
+    @Param('id') id: number,
+    @Body() { brand, description, name, price_range, seller }: UpdateProductDto,
+  ) {
+    const updateInput: Prisma.ProductUpdateInput = {};
+
+    if (brand) {
+      updateInput.brand = {
+        connectOrCreate: {
+          where: { name: brand },
+          create: { name: brand },
+        },
+      };
+    }
+
+    if (seller) {
+      updateInput.seller = {
+        connectOrCreate: {
+          where: { name: seller },
+          create: { name: seller },
+        },
+      };
+    }
+
+    if (description) {
+      updateInput.description = description;
+    }
+
+    if (name) {
+      updateInput.name = name;
+    }
+
+    if (price_range) {
+      updateInput.price_range = price_range;
+    }
+
+    return this.productsService.update(id, updateInput);
   }
 
   @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.productsService.remove(+id);
+  remove(@Param('id') id: number) {
+    return this.productsService.remove(id);
   }
 }
--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -1,26 +1,39 @@
 import { Injectable } from '@nestjs/common';
-import { CreateProductDto } from './dto/create-product.dto';
-import { UpdateProductDto } from './dto/update-product.dto';
+import { Prisma } from '@prisma/client';
+import { PrismaService } from '../services/prisma.service';
 
 @Injectable()
 export class ProductsService {
-  create(createProductDto: CreateProductDto) {
-    return 'This action adds a new product';
+  constructor(private readonly prisma: PrismaService) {}
+
+  async create(data: Prisma.ProductCreateInput) {
+    return this.prisma.product.create({ data });
   }
 
-  findAll() {
-    return `This action returns all products`;
+  async findAll() {
+    return this.prisma.product.findMany();
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} product`;
+  async find(args: Prisma.ProductFindManyArgs) {
+    return this.prisma.product.findMany(args);
   }
 
-  update(id: number, updateProductDto: UpdateProductDto) {
-    return `This action updates a #${id} product`;
+  async findById(id: number) {
+    return this.prisma.product.findUnique({ where: { id } });
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} product`;
+  async update(id: number, data: Prisma.ProductUpdateInput) {
+    return this.prisma.product.update({
+      where: { id },
+      data,
+      include: {
+        brand: true,
+        seller: true,
+      },
+    });
+  }
+
+  async remove(id: number) {
+    return this.prisma.product.delete({ where: { id } });
   }
 }
