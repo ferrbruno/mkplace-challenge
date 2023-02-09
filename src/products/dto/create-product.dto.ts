9045bb45011066b5c4edab95499bdcec2ac4bbb9
Bruno Ferreira <bruno.ferr.dev@gmail.com>
2023-02-09 19:20:18 -0300

    feat: added global validator :sparkles:

--- src/products/dto/create-product.dto.ts
+++ src/products/dto/create-product.dto.ts
@@ -1,1 +1,29 @@
-export class CreateProductDto {}
+import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
+import { IsNumber, IsString, IsOptional, IsNotEmpty } from 'class-validator';
+
+export class CreateProductDto {
+  @ApiProperty()
+  @IsString()
+  @IsNotEmpty()
+  name: string;
+
+  @ApiProperty()
+  @IsNumber()
+  price_range: number;
+
+  @ApiPropertyOptional()
+  @IsString()
+  @IsNotEmpty()
+  @IsOptional()
+  description?: string;
+
+  @ApiProperty()
+  @IsString()
+  @IsNotEmpty()
+  brand: string;
+
+  @ApiProperty()
+  @IsString()
+  @IsNotEmpty()
+  seller: string;
+}
