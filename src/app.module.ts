300bcd2eb5c0b9cb29e14caf9b6e4e5eaa1868f9
Bruno Ferreira <bruno.ferr.dev@gmail.com>
2023-02-10 13:56:01 -0300

    feat: added redis cache :sparkles:

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,32 @@
-import { Module } from '@nestjs/common';
+import { CacheModule, Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { APP_FILTER } from '@nestjs/core';
+import { redisStore } from 'cache-manager-ioredis-yet';
+import { type RedisOptions } from 'ioredis';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ProductsModule } from './products/products.module';
 import { BrandsModule } from './brands/brands.module';
-import { SellersModule } from './sellers/sellers.module';
 import { PrismaExceptionFilter } from './prisma/prisma.filter';
+import { ProductsModule } from './products/products.module';
+import { SellersModule } from './sellers/sellers.module';
 
 @Module({
-  imports: [ProductsModule, BrandsModule, SellersModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    CacheModule.registerAsync<RedisOptions>({
+      isGlobal: true,
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => ({
+        store: redisStore,
+        host: config.getOrThrow('REDIS_HOST'),
+      }),
+    }),
+    ProductsModule,
+    BrandsModule,
+    SellersModule,
+  ],
   controllers: [AppController],
   providers: [
     AppService,
