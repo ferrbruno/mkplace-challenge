6d7ac83ff6e303da6d39cd52c67a797d0f918dc1
Bruno Ferreira <bruno.ferr.dev@gmail.com>
2023-02-10 10:20:28 -0300

    feat: added sellers module :sparkles:

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ProductsModule } from './products/products.module';
 import { BrandsModule } from './brands/brands.module';
+import { SellersModule } from './sellers/sellers.module';
 
 @Module({
-  imports: [ProductsModule, BrandsModule],
+  imports: [ProductsModule, BrandsModule, SellersModule],
   controllers: [AppController],
   providers: [AppService],
 })
